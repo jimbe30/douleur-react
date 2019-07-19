48665cd7ae6ca9962abeb38b4c3fe42d96cef724
JMB <jean-marc.brun@cegedim-activ.com>
2019-07-19 16:47:35 +0200

    initialisation appli-douleur

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+
+import MainApp from './App'
+
+ReactDOM.render( <MainApp />, document.getElementById('react-app'))
