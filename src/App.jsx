2cc065b65379294db276441f3f19a071005edd23
JMB <jean-marc.brun@cegedim-activ.com>
2019-10-17 19:29:37 +0200

    Vue arborescence douleur

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,16 @@
 import React from 'react'
-import { Accordion, AccordionTitle } from 'semantic-ui-react'
+import { NomenclatureRestClient } from './restclients/NomenclatureRestClient'
+import "./App.css";
+import Test from './Test';
 
 
 export default function MainApp() {
 
-    const nomenclature = [{
-        type: "Douleur aigüe",
-        sousTypes: [{
-            type: "Post opératoire",
-            sousTypes: [{
-                type: "Cardio",
-                sousTypes : ["Coronnaire", "Valvulaire", "Périphérique"] 
-            },{
-                type: "Gynéco"
-            },{
-                type: "Ortho"
-            },{
-                type: "Viscéral"
-            }
-            ]},{
-            type: "Traumatique"
-        }]},{
-            type: "Douleur chronique"
-        },{
-            type: "Douleur neuropathique"
-    }]
-
-    function Entree({titre, lien}) {
-        const contenu = lien ? <a href={lien}>{titre}</a> : <div>{titre}</div>        
-        return (
-            <Accordion><AccordionTitle>{contenu}</AccordionTitle></Accordion>
-        )
-    }
-
-    function Branche({titre, entrees}) {
-        const retour = [{
-            title: titre,
-            content: {}
-        }]
-        if (Array.isArray(entrees)) {
-            var entreesAffichables = entrees.map(entree => {
-                var sTitre = entree
-                if (entree.type) {
-                    sTitre = entree.type
-                }
-                if (entree.sousTypes) {
-                    return <Branche titre={sTitre} entrees={entree.sousTypes}/>
-                } else {
-                    return <Entree titre={sTitre}/>
-                }   
-            });
-            var content = <div>{entreesAffichables}</div>
-            retour[0].content = {content};
-        } 
-        return <Accordion styled panels={retour} />
-    }
-
-    function Arborescence({nomenclature}) {
-        return nomenclature.map(douleur => {
-            return <Branche titre={douleur.type} entrees={douleur.sousTypes} />  
-        })
-    }
-
     return (
         <div>
-            <Arborescence nomenclature={nomenclature} />
+            <NomenclatureRestClient/>
+
+            <Test/>
         </div>
     )
 }
